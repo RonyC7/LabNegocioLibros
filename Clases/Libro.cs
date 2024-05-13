856f38043dd78fc9f7e85a50f0d80da00b99db61
RonyC7 <carpioalvaradorony@mesoamericana.edu.gt>
2024-05-13 08:11:37 -0600

    Creacion de la clase

---
+++ Clases/Libro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LabNegocioLibros.Clases
+{
+    public class Libro
+    {
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public decimal PrecioCompra { get; set; }
+        public decimal PrecioVenta { get; set; }
+        public string Editorial { get; set; }
+        public string Categoria { get; set; }
+        public string Autor { get; set; }
+    }
+}
