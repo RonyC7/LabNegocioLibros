c555d5dc7647b28bda61aba652da79d869343a75
RonyC7 <carpioalvaradorony@mesoamericana.edu.gt>
2024-05-13 08:17:04 -0600

    Creacion de clase para poder agregar y obtener libros

---
+++ Clases/LibroService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace LabNegocioLibros.Clases
+{
+    public class LibroService
+    {
+        private List<Libro> libros = new List<Libro>();
+
+        public void AgregarLibro(Libro libro)
+        {
+            libros.Add(libro);
+        }
+
+        public List<Libro> ObtenerLibros()
+        {
+            return libros;
+        }
+    }
+}
